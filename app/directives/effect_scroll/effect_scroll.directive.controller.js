85455723b2e597f182c8ac28daca05f09f2f05ea
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-08-31 21:56:57 +0300

    add scroll button, update homepage sections, footer

--- app/directives/effect_scroll/effect_scroll.directive.controller.js
+++ app/directives/effect_scroll/effect_scroll.directive.controller.js
@@ -11,20 +11,21 @@
     function effectScrollController($state) {
         var vm = this;
 
-        $("#anchor").on("click", "a", function (event) {
+        vm.scroll = scroll;
 
+        function scroll() {
             event.preventDefault();
-            var id = $(this).attr('href'),
-                top = $(id).offset().top;
-            // console.log(id);
-            if (id === '#section') {
-                top = top - 200;
-                // return top;
+
+            var top = $('#section').offset().top;
+            if (top === '#section') {
+                top = top - 150;
                 $('body,html').animate({scrollTop: top}, 1000);
             }
 
             $('body,html').animate({scrollTop: top}, 1000);
-        });
+
+        }
+
 
 
 
--- app/templates/homepage/homepage.controller.js
+++ app/templates/homepage/homepage.controller.js
@@ -18,55 +18,55 @@
                 link:'ecn'
             },
             {
-                image:'content/img/homepage_solution/4 (2).png',
+                image:'content/img/homepage_solution/2.png',
                 title:'homepage_solution_item2_title',
                 text:'homepage_solution_item2_text',
                 link:'mt4'
             },
             {
-                image:'content/img/homepage_solution/2.png',
+                image:'content/img/homepage_solution/3.png',
                 title:'homepage_solution_item3_title',
                 text:'homepage_solution_item3_text',
                 link:'liquidity'
             },
             {
-                image:'content/img/homepage_solution/3.png',
+                image:'content/img/homepage_solution/4.png',
                 title:'homepage_solution_item4_title',
                 text:'homepage_solution_item4_text',
                 link:'trade'
             },
             {
-                image:'content/img/homepage_solution/5 (2).png',
+                image:'content/img/homepage_solution/5.png',
                 title:'homepage_solution_item5_title',
                 text:'homepage_solution_item5_text',
                 link:'provider'
             },
             {
-                image:'content/img/homepage_solution/3.png',
+                image:'content/img/homepage_solution/6.png',
                 title:'homepage_solution_item6_title',
                 text:'homepage_solution_item6_text',
                 link:'floating'
             },
             {
-                image:'content/img/homepage_solution/6.png',
+                image:'content/img/homepage_solution/7.png',
                 title:'homepage_solution_item7_title',
                 text:'homepage_solution_item7_text',
                 link:'auto_risk'
             },
             {
-                image:'content/img/homepage_solution/4 (2).png',
+                image:'content/img/homepage_solution/8.png',
                 title:'homepage_solution_item8_title',
                 text:'homepage_solution_item8_text',
                 link:'api'
             },
             {
-                image:'content/img/homepage_solution/1.png',
+                image:'content/img/homepage_solution/9.png',
                 title:'homepage_solution_item9_title',
                 text:'homepage_solution_item9_text',
                 link:'trader_room'
             },
             {
-                image:'content/img/homepage_solution/3.png',
+                image:'content/img/homepage_solution/10.png',
                 title:'homepage_solution_item10_title',
                 text:'homepage_solution_item10_text',
                 link:'crm'
